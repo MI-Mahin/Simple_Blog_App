35d33a4514f6ba8e3cf0e21bfecb4779b1ed8ac0
MI-Mahin <mahmudulislammahin07@gmail.com>
2025-08-03 19:07:39 +0600

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000' });
+const API = axios.create({ baseURL: 'https://simpleblogapp-production.up.railway.app' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
